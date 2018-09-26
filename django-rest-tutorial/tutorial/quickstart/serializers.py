7d1f9949fd299799fbadcfab0898ea8a5fb11f5a
niceasitaka <niceasitaka@naver.com>
2018-09-26 15:22:13 +0900

    주석 내용 추가

--- django-rest-tutorial/tutorial/quickstart/serializers.py
+++ django-rest-tutorial/tutorial/quickstart/serializers.py
@@ -1,3 +1,7 @@
+# Serializer는 queryset과 모델 인스턴스와 같은 복잡한 데이터를 
+# JSON, XML 또는 다른 콘텐츠 유형으로 쉽게 변환 가능
+# ModelForm 클래스와 유사하게 동작함
+
 from django.contrib.auth.models import User, Group
 
 from rest_framework import serializers
--- django-rest-tutorial/tutorial/quickstart/views.py
+++ django-rest-tutorial/tutorial/quickstart/views.py
@@ -11,12 +11,13 @@ class UserViewSet(viewsets.ModelViewSet):
 
 # 그룹을 보거나 편집하는 API
 class GroupViewSet(viewsets.ModelViewSet):
-	queryset = User.objects.all()
+	queryset = Group.objects.all()
 	serializer_class = GroupSerializer
 	
 '''
 일반적인 경우 ViewSet 클래스에 model 속성만 설정하면 serializer와 queryset은 
-자동으로 설정됩니다. queryset이나 serializer_class 속성을 설정하면 API의 행동을 좀더 
-명시적으로 표현할 수 있는데 대다수 애플리케이션에서는 이렇게 하기를 권장합니다.
+자동으로 설정.
+queryset이나 serializer_class 속성을 설정하면 API의 행동을 좀더 
+명시적으로 표현할 수 있는데 대다수 애플리케이션에서는 이렇게 하기를 권장.
 '''
 
