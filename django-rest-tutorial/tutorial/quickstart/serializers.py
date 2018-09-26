cb2b1c6c30bc835c740c5b9557d4db8ac40d45e1
niceasitaka <niceasitaka@naver.com>
2018-09-26 15:47:50 +0900

    주석 내용 추가

--- django-rest-tutorial/tutorial/quickstart/serializers.py
+++ django-rest-tutorial/tutorial/quickstart/serializers.py
@@ -1,7 +1,3 @@
-# Serializer는 queryset과 모델 인스턴스와 같은 복잡한 데이터를 
-# JSON, XML 또는 다른 콘텐츠 유형으로 쉽게 변환 가능
-# ModelForm 클래스와 유사하게 동작함
-
 from django.contrib.auth.models import User, Group
 
 from rest_framework import serializers
@@ -15,3 +11,9 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = Group
 		fields = ['url', 'name']
+		
+'''
+Serializer는 queryset과 모델 인스턴스와 같은 복잡한 데이터를 
+JSON, XML 또는 다른 콘텐츠 유형으로 쉽게 변환 가능
+ModelForm 클래스와 유사하게 동작함
+'''
