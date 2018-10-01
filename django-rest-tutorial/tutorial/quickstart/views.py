f6cacc9973fd0964fb7b2f84b059328bfb40aa93
niceasitaka <niceasitaka@naver.com>
2018-10-02 00:41:55 +0900

    django-rest-tutorial 두번째 튜토리얼 시작

--- django-rest-tutorial/tutorial/quickstart/views.py
+++ django-rest-tutorial/tutorial/quickstart/views.py
@@ -19,5 +19,12 @@ class GroupViewSet(viewsets.ModelViewSet):
 자동으로 설정.
 queryset이나 serializer_class 속성을 설정하면 API의 행동을 좀더 
 명시적으로 표현할 수 있는데 대다수 애플리케이션에서는 이렇게 하기를 권장.
+
+viewsets 클래스를 사용하면 view 클래스보다 좋은점
+- routers를 통해 더이상 사용자 스스로 URL 설정파일을 다룰 필요가 없음
+
+뷰를 여러 번 작성하기보다는 자주 사용되는 기능들을 묶은 ViewSet 클래스를 사용
+
+물론 필요에 따라 뷰를 잘게 나눌 수도 있습니다. 하지만 뷰셋(viewset)을 사용하면 뷰 로직을 아주 간결하고 체계적으로 유지 가능
 '''
 
